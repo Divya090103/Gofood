b26ce892e2aa71d1a97c768a44536ce219a89e25
09div <divya09ag@gmail.com>
2024-07-28 14:03:31 +0530

    get data from database and show on the page

--- Backend/Routes/displaydata.js
+++ Backend/Routes/displaydata.js
@@ -3,13 +3,24 @@ const Routes = express.Router();
 const mongoose = require("mongoose");
 const connect = require("../database/db.js");
 Routes.post("/food_data", async (req, res) => {
-  try{
-    const data=await mongoose.connection.db.collection("data");
-    const fetched=await data.find({}).toArray();
-    console.log(fetched);
+  try {
+    const data = await mongoose.connection.db.collection("data");
+    const fetched = await data.find({}).toArray();
+
     return res.send([fetched]);
-  }catch(e){
+  } catch (e) {
     console.log(e);
   }
 });
+Routes.post("/category",async(req,res)=>{
+  try{
+    const category = await mongoose.connection.db.collection("Category");
+    const fetched=await category.find({}).toArray();
+    return res.send([fetched]);
+
+  }catch(e){
+    return res.send(e);
+  }
+
+})
 module.exports = Routes;
